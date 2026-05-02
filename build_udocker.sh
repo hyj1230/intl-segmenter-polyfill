a5246d2b3397842fabed503e51d0c734af72f0e1
hyj1230 <121798333+hyj1230@users.noreply.github.com>
2026-05-03 00:26:00 +0800

    创建 build_udocker.sh

---
+++ build_udocker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+udocker build . --file Dockerfile.icu -t icu-data
+
+rm -Rf build && mkdir build
+
+udocker run -v "$PWD/build:/opt/mount" --rm "$(docker images -q icu-data)" cp /artifacts/data.h /opt/mount
+cp break_iterator.c icu.py build/
+udocker build . --file Dockerfile -t build
+
+udocker run -v "$PWD/src:/opt/mount" --rm "$(docker images -q build)" cp /artifacts/break_iterator.wasm /opt/mount
